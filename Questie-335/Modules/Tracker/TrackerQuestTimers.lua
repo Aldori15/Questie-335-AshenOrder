24c1808e91d994488af49f528eeb0bb8fc2ddd7c
Aldori <m.baxter1122@yahoo.com>
2026-03-02 16:01:19 -0500

    fix: Fix possible init error for quest timers

--- Questie-335/Modules/Tracker/TrackerQuestTimers.lua
+++ Questie-335/Modules/Tracker/TrackerQuestTimers.lua
@@ -160,12 +160,19 @@ end
 
 function TrackerQuestTimers:UpdateTimerFrame()
     if timer and (Questie.db.profile.trackerEnabled and Questie.db.char.isTrackerExpanded and (QuestieTracker.disableHooks ~= true)) then
-        local timeRemainingString, timeRemaining = TrackerQuestTimers:GetRemainingTimeByQuestId(timer.questId)
+        local timeRemainingString = TrackerQuestTimers:GetRemainingTimeByQuestId(timer.questId)
         if timeRemainingString ~= nil then
             Questie:Debug(Questie.DEBUG_SPAM, "[TrackerQuestTimers:UpdateTimerFrame] - ", timeRemainingString)
-            timer.frame.label:SetFont(LSM30:Fetch("font", Questie.db.profile.trackerFontObjective), Questie.db.profile.trackerFontSizeObjective, Questie.db.profile.trackerFontOutline)
-            timer.frame.label:SetText(Questie:Colorize(timeRemainingString, "blue"))
-            timer.frame:SetWidth(timer.frame.label:GetWidth() + ((34) - (18 - Questie.db.profile.trackerFontSizeQuest)) + Questie.db.profile.trackerFontSizeQuest)
+
+            QuestieCombatQueue:Queue(function()
+                if (not timer) then
+                    -- timer might be reset on next combat queue
+                    return
+                end
+                timer.frame.label:SetFont(LSM30:Fetch("font", Questie.db.profile.trackerFontObjective), Questie.db.profile.trackerFontSizeObjective, Questie.db.profile.trackerFontOutline)
+                timer.frame.label:SetText(Questie:Colorize(timeRemainingString, "lightBlue"))
+                timer.frame:SetWidth(timer.frame.label:GetWidth() + ((34) - (18 - Questie.db.profile.trackerFontSizeQuest)) + Questie.db.profile.trackerFontSizeQuest)
+            end)
         else
             Questie:Debug(Questie.DEBUG_SPAM, "[TrackerQuestTimers] Quest Timer Expired!")
             return
