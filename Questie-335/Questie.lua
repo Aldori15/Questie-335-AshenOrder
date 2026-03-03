e18ab9502ae66da165c43a38590811f9fa78c06b
Aldori <m.baxter1122@yahoo.com>
2026-03-03 16:11:18 -0500

    Add a few more icon mappings

--- Questie-335/Questie.lua
+++ Questie-335/Questie.lua
@@ -188,6 +188,10 @@ Questie.icons = {
     ["tracker_quests"] = QuestieLib.AddonPath.."Icons\\tracker_quests.tga",
     ["tracker_search"] = QuestieLib.AddonPath.."Icons\\tracker_search.tga",
     ["tracker_settings"] = QuestieLib.AddonPath.."Icons\\tracker_settings.tga",
+    ["node_fish"] = QuestieLib.AddonPath.."Icons\\node_fish.blp",
+    ["node_herb"] = QuestieLib.AddonPath.."Icons\\node_herb.blp",
+    ["node_ore"] = QuestieLib.AddonPath.."Icons\\node_ore.blp",
+    ["chest"] = QuestieLib.AddonPath.."Icons\\chest.blp",
 }
 
 Questie.usedIcons = {}
@@ -210,6 +214,11 @@ Questie.ICON_TYPE_PVPQUEST = 15
 Questie.ICON_TYPE_PVPQUEST_COMPLETE = 16
 Questie.ICON_TYPE_INTERACT = 17
 Questie.ICON_TYPE_SODRUNE = 18
+Questie.ICON_TYPE_MOUNT_UP = 19
+Questie.ICON_TYPE_NODE_FISH = 20
+Questie.ICON_TYPE_NODE_HERB = 21
+Questie.ICON_TYPE_NODE_ORE = 22
+Questie.ICON_TYPE_CHEST = 23
 
 -- Load icon pathes from SavedVariables or set the default ones
 function Questie:SetIcons()
@@ -231,6 +240,11 @@ function Questie:SetIcons()
     Questie.usedIcons[Questie.ICON_TYPE_PVPQUEST_COMPLETE] = Questie.db.profile.ICON_PVPQUEST_COMPLETE or Questie.icons["complete"]
     Questie.usedIcons[Questie.ICON_TYPE_INTERACT] = Questie.db.profile.ICON_TYPE_INTERACT or Questie.icons["interact"]
     Questie.usedIcons[Questie.ICON_TYPE_SODRUNE] = Questie.icons["sod_rune"]
+    Questie.usedIcons[Questie.ICON_TYPE_MOUNT_UP] = Questie.icons["mount_up"]
+    Questie.usedIcons[Questie.ICON_TYPE_NODE_FISH] = Questie.icons["node_fish"]
+    Questie.usedIcons[Questie.ICON_TYPE_NODE_HERB] = Questie.icons["node_herb"]
+    Questie.usedIcons[Questie.ICON_TYPE_NODE_ORE] = Questie.icons["node_ore"]
+    Questie.usedIcons[Questie.ICON_TYPE_CHEST] = Questie.icons["chest"]
 end
 
 function Questie:GetIconNameFromPath(path)
