37ef54012abb5c36b876d6ac0baf583f94959e14
Aldori <m.baxter1122@yahoo.com>
2026-04-01 10:00:56 -0400

    fix: Refresh profession quests every 5 skill points

--- Questie-335/Modules/QuestieProfessions.lua
+++ Questie-335/Modules/QuestieProfessions.lua
@@ -79,9 +79,14 @@ function QuestieProfessions:Update()
 
             --? Reset all autoBlacklisted quests if a new skill is learned
             QuestieQuest.ResetAutoblacklistCategory("skill")
-        elseif temporaryPlayerProfessions[professionId][2] > playerProfessions[professionId][2] then
-            Questie:Debug(Questie.DEBUG_DEVELOP, "Profession update: " .. temporaryPlayerProfessions[professionId][1] .. " " .. playerProfessions[professionId][2] .. " -> " .. temporaryPlayerProfessions[professionId][2])
-            hasProfessionUpdate = true -- A profession leveled up, not something like "Defense"
+        else
+            local oldRank = playerProfessions[professionId][2]
+            local newRank = temporaryPlayerProfessions[professionId][2]
+            if newRank > oldRank and (math.floor(oldRank / 5) ~= math.floor(newRank / 5)) then
+                -- We only want to update every 5 skill levels because all other progressions won't unlock new quests
+                Questie:Debug(Questie.DEBUG_DEVELOP, "Profession update: " .. temporaryPlayerProfessions[professionId][1] .. " " .. oldRank .. " -> " .. newRank)
+                hasProfessionUpdate = true
+            end
         end
     end
     playerProfessions = temporaryPlayerProfessions
