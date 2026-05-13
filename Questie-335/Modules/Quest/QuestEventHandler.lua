9109c57f779c1526db5513c03311a599a8313052
Aldori <m.baxter1122@yahoo.com>
2026-05-13 10:46:20 -0400

    fix: Correct logic for breadcrumb announces
    The earlier commit made the condition too strict.

--- Questie-335/Modules/Quest/QuestEventHandler.lua
+++ Questie-335/Modules/Quest/QuestEventHandler.lua
@@ -238,7 +238,7 @@ function _QuestEventHandler:QuestAccepted(questLogIndex, questId)
                         end
                     end
 
-                    if QuestiePlayer.HasRequiredRace(requiredRaces) and QuestiePlayer.HasRequiredClass(requiredClasses) and (not exclusiveQuestCompleted) and (availableUntilCompleted ~= 0 and not Questie.db.char.complete[availableUntilCompleted]) then
+                    if QuestiePlayer.HasRequiredRace(requiredRaces) and QuestiePlayer.HasRequiredClass(requiredClasses) and (not exclusiveQuestCompleted) and (not Questie.db.char.complete[availableUntilCompleted]) then
                         QuestieAnnounce.IncompleteBreadcrumbQuest(questId, breadcrumbQuestId)
                     end
                 end
