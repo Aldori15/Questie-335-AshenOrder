ace145fb4bd9acee4d60386f2fa3c6a72cfced0c
Aldori <m.baxter1122@yahoo.com>
2026-05-15 17:04:15 -0400

    fix: Make ZoneDB.GetUiMapIdByAreaId nil safe

--- Questie-335/Database/Zones/zoneDB.lua
+++ Questie-335/Database/Zones/zoneDB.lua
@@ -89,7 +89,7 @@ end
 function ZoneDB:GetUiMapIdByAreaId(areaId)
     local uiMapId = areaIdToUiMapId[areaId] or specialZoneIdToUiMapId[areaId]
     if (not uiMapId) then
-        Questie:Debug(Questie.DEBUG_CRITICAL, "No UiMapId found for AreaId: " .. areaId)
+        Questie:Debug(Questie.DEBUG_CRITICAL, "No UiMapId found for AreaId: " .. tostring(areaId))
     end
 
     return uiMapId
