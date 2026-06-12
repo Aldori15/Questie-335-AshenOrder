beaca44206bb64a83b33a7b6ca9000fe1a7163f2
Aldori <m.baxter1122@yahoo.com>
2026-06-12 11:31:08 -0400

    refactor(compat): extract event compatibility layer

--- Questie-335/Compat/Compat.lua
+++ Questie-335/Compat/Compat.lua
@@ -4,12 +4,8 @@ local QuestieLib = QuestieLoader:ImportModule("QuestieLib")
 local QuestieDB = QuestieLoader:ImportModule("QuestieDB")
 ---@type QuestieOptions
 local QuestieOptions = QuestieLoader:ImportModule("QuestieOptions")
----@type QuestieEventHandler
-local QuestieEventHandler = QuestieLoader:ImportModule("QuestieEventHandler")
 ---@type QuestieQuest
 local QuestieQuest = QuestieLoader:ImportModule("QuestieQuest")
----@type QuestEventHandler
-local QuestEventHandler = QuestieLoader:ImportModule("QuestEventHandler")
 ---@type ZoneDB
 local ZoneDB = QuestieLoader:ImportModule("ZoneDB")
 ---@class QuestieCoords
@@ -443,43 +439,6 @@ function QuestieCompat.GetClassColor(classFilename)
 	return 1, 1, 1, "ffffffff";
 end
 
-local _EventHandler = QuestieEventHandler.private
-function QuestieCompat.QuestieEventHandler_RegisterLateEvents()
-    -- In fullscreen mode, WorldMap intercepts keyboard input,
-    -- preventing the MODIFIER_STATE_CHANGED event
-    if WorldMapFrame:GetScript("OnKeyDown") then
-        local modifierStateChanged
-        WorldMapFrame:HookScript("OnKeyDown", function(self, key)
-            if IsModifierKeyDown() then
-                _EventHandler:ModifierStateChanged(key, 1)
-                modifierStateChanged = true
-            end
-        end)
-        WorldMapFrame:HookScript("OnKeyUp", function(self, key)
-            if modifierStateChanged then
-                _EventHandler:ModifierStateChanged(key, 0)
-                modifierStateChanged = nil
-            end
-        end)
-    end
-
-    Questie:UnregisterEvent("MAP_EXPLORATION_UPDATED") -- https://wowpedia.fandom.com/wiki/MAP_EXPLORATION_UPDATED
-    Questie:UnregisterEvent("NEW_RECIPE_LEARNED")
-
-    -- Party join event for QuestieComms, Use bucket to hinder this from spamming (Ex someone using a raid invite addon etc)
-    Questie:UnregisterEvent("GROUP_ROSTER_UPDATE") -- https://wowpedia.fandom.com/wiki/GROUP_ROSTER_UPDATE
-    Questie:UnregisterEvent("GROUP_JOINED") -- https://wowpedia.fandom.com/wiki/GROUP_JOINED
-    Questie:UnregisterEvent("GROUP_LEFT") -- https://wowpedia.fandom.com/wiki/GROUP_LEFT
-    Questie:RegisterEvent("PARTY_MEMBERS_CHANGED", QuestieCompat.GroupRosterUpdate)
-    Questie:RegisterBucketEvent("RAID_ROSTER_UPDATE", 1, QuestieCompat.GroupRosterUpdate)
-
-    QuestieCompat.RegisterNameplateCompatibilityEvents()
-
-    -- Use loot events as additional sync points for tracker refreshes.
-    Questie:RegisterEvent("LOOT_SLOT_CLEARED", QuestieCompat.LOOT_SLOT_CLEARED)
-    Questie:RegisterEvent("LOOT_CLOSED", QuestieCompat.LOOT_CLOSED)
-end
-
 -- prevents the override of existing global variables with the same name(e.g., WorldMapButton)
 function QuestieCompat.PopulateGlobals(self)
     for name, module in pairs(QuestieLoader._modules) do
@@ -728,7 +687,6 @@ function QuestieCompat:ADDON_LOADED(event, addon)
     QuestieLib.TextWrap = QuestieCompat.TextWrap
     QuestieCoords.GetPlayerMapPosition = QuestieCompat.GetPlayerMapPosition
     QuestieCoords.ResetMiniWorldMapText = QuestieCompat.NOOP
-    QuestieEventHandler.private.UiInfoMessage = QuestieCompat.UiInfoMessage
     QuestieCompat.orig_QuestieOptions_Initialize = QuestieOptions.Initialize
     QuestieOptions.Initialize = QuestieCompat.QuestieOptions_Initialize
     QuestieCompat.orig_GetSelectedSoundFile = Sounds.GetSelectedSoundFile
@@ -737,8 +695,7 @@ function QuestieCompat:ADDON_LOADED(event, addon)
 	QuestieLink.GetQuestLinkStringById = rawget(QuestieLink, "GetQuestLinkStringById") or QuestieCompat.GetQuestLinkStringById
 	QuestieLink.GetQuestHyperLink = rawget(QuestieLink, "GetQuestHyperLink") or QuestieCompat.GetQuestLinkStringById
 
-    hooksecurefunc(QuestieEventHandler, "RegisterLateEvents", QuestieCompat.QuestieEventHandler_RegisterLateEvents)
-    hooksecurefunc(QuestEventHandler, "RegisterEvents", QuestieCompat.QuestEventHandler_RegisterEvents)
+    QuestieCompat.RegisterEventCompatibilityHooks()
     QuestieCompat.RegisterTrackerCompatibilityHooks()
     hooksecurefunc(QuestieQuest, "ToggleNotes", QuestieCompat.HBDPins.UpdateWorldMap)
 	hooksecurefunc("ReloadUI", QuestieCompat.OnReloadUi)
