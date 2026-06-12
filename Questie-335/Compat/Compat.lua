a7ae62800136c2a28ea37d5613a4dcedf9e310df
Aldori <m.baxter1122@yahoo.com>
2026-06-12 11:23:20 -0400

    refactor(compat): extract stream compatibility layer

--- Questie-335/Compat/Compat.lua
+++ Questie-335/Compat/Compat.lua
@@ -1,7 +1,5 @@
 ---@type QuestieLib
 local QuestieLib = QuestieLoader:ImportModule("QuestieLib")
----@type QuestieStream
-local QuestieStream = QuestieLoader:ImportModule("QuestieStreamLib")
 ---@type QuestieDB
 local QuestieDB = QuestieLoader:ImportModule("QuestieDB")
 ---@type QuestieOptions
@@ -445,95 +443,6 @@ function QuestieCompat.GetClassColor(classFilename)
 	return 1, 1, 1, "ffffffff";
 end
 
---[[
-    xpcall wrapper implementation
-]]
-local xpcall = xpcall
-
-local function errorhandler(err)
-	return geterrorhandler()(err)
-end
-
-local function CreateDispatcher(argCount)
-	local code = [[
-		local xpcall, errorhandler = ...
-		local method, ARGS
-		local function call() return method(ARGS) end
-
-		local function dispatch(func, eh, ...)
-			 method = func
-			 if not method then return end
-			 ARGS = ...
-			 return xpcall(call, eh or errorhandler)
-		end
-
-		return dispatch
-	]]
-
-	local ARGS = {}
-	for i = 1, argCount do ARGS[i] = "arg"..i end
-	code = code:gsub("ARGS", table.concat(ARGS, ", "))
-	return assert(loadstring(code, "safecall Dispatcher["..argCount.."]"))(xpcall, errorhandler)
-end
-
-local Dispatchers = setmetatable({}, {__index=function(self, argCount)
-	local dispatcher = CreateDispatcher(argCount)
-	rawset(self, argCount, dispatcher)
-	return dispatcher
-end})
-
-Dispatchers[0] = function(func, eh)
-	return xpcall(func, eh or errorhandler)
-end
-
-function QuestieCompat.xpcall(func, eh, ...)
-    if type(func) == "function" then
-		return Dispatchers[select('#', ...)](func, eh, ...)
-	end
-end
-
---[[
-    It seems that the table size is capped in 3.3.5, with a maximum of 524,288 entries.
-    For instance, this code triggers an error message: 'memory allocation error: block too big.
-
-    local t = {}
-	for i=1, 524289 do
-		t[i] = true
-	end
-
-    Spliting the table into multiple subtables should do the trick.
-]]
-
-local stringchar = string.char
-local MAX_TABLE_SIZE = 524288
-
-function QuestieCompat._writeByte(self, val)
-	local subIndex = math.ceil(self._pointer / MAX_TABLE_SIZE)
-	local index = self._pointer - (subIndex - 1) * MAX_TABLE_SIZE
-
-	self._bin[subIndex] = self._bin[subIndex] or {}
-	self._bin[subIndex][index] = stringchar(val)
-
-    self._pointer = self._pointer + 1
-end
-
-function QuestieCompat._readByte(self)
-	local subIndex = math.ceil(self._pointer / MAX_TABLE_SIZE)
-	local index = self._pointer - (subIndex - 1) * MAX_TABLE_SIZE
-
-    self._pointer = self._pointer + 1
-
-	return self._bin[subIndex][index]
-end
-
-function QuestieCompat.Save(self)
-	local result = ""
-	for i=1, #self._bin do
-		result = result .. table.concat(self._bin[i])
-	end
-	return result
-end
-
 local _EventHandler = QuestieEventHandler.private
 function QuestieCompat.QuestieEventHandler_RegisterLateEvents()
     -- In fullscreen mode, WorldMap intercepts keyboard input,
@@ -814,9 +723,7 @@ function QuestieCompat:ADDON_LOADED(event, addon)
     end
 
 	QuestieLoader.PopulateGlobals = QuestieCompat.PopulateGlobals
-    QuestieStream._writeByte = QuestieCompat._writeByte
-    QuestieStream._readByte = QuestieCompat._readByte
-    QuestieStream.Save = QuestieCompat.Save
+    QuestieCompat.RegisterStreamCompatibility()
     ZoneDB.private.RunTests = QuestieCompat.NOOP
     QuestieLib.TextWrap = QuestieCompat.TextWrap
     QuestieCoords.GetPlayerMapPosition = QuestieCompat.GetPlayerMapPosition
@@ -836,7 +743,6 @@ function QuestieCompat:ADDON_LOADED(event, addon)
     hooksecurefunc(QuestieQuest, "ToggleNotes", QuestieCompat.HBDPins.UpdateWorldMap)
 	hooksecurefunc("ReloadUI", QuestieCompat.OnReloadUi)
 	hooksecurefunc("ConsoleExec", QuestieCompat.OnReloadUi)
-
 
     local Mapster = LibStub("AceAddon-3.0"):GetAddon("Mapster", true)
     if Mapster and Mapster.RefreshQuestObjectivesDisplay then
