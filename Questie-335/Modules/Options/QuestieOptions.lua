55978430e4801cab77e58c6a5899030e079d810e
Aldori <m.baxter1122@yahoo.com>
2026-06-16 12:08:20 -0400

    chore(options): remove unused option tabs

--- Questie-335/Modules/Options/QuestieOptions.lua
+++ Questie-335/Modules/Options/QuestieOptions.lua
@@ -133,10 +133,6 @@ end
 _CreateOptionsTable = function()
     local general_tab = QuestieOptions.tabs.general:Initialize()
     coroutine.yield()
-    --local social_tab = QuestieOptions.tabs.social:Initialize()
-    --coroutine.yield()
-    --local minimap_tab = QuestieOptions.tabs.minimap:Initialize()
-    --coroutine.yield()
     local map_tab = QuestieOptions.tabs.map:Initialize()
     coroutine.yield()
     local icons_tab = QuestieOptions.tabs.icons:Initialize()
@@ -145,13 +141,7 @@ _CreateOptionsTable = function()
     coroutine.yield()
     local auto_tab = QuestieOptions.tabs.auto:Initialize()
     coroutine.yield()
-    --local tooltip_tab = QuestieOptions.tabs.tooltip:Initialize()
-    --coroutine.yield()
-    --local sounds_tab = QuestieOptions.tabs.sounds:Initialize()
-    --coroutine.yield()
     local nameplate_tab = QuestieOptions.tabs.nameplate:Initialize()
-    coroutine.yield()
-    local dbm_hud_tab = QuestieOptions.tabs.dbm:Initialize()
     coroutine.yield()
     local advanced_tab = QuestieOptions.tabs.advanced:Initialize()
     coroutine.yield()
@@ -162,16 +152,11 @@ _CreateOptionsTable = function()
         childGroups = "tab",
         args = {
             general_tab = general_tab,
-            --social_tab = social_tab,
-            --minimap_tab = minimap_tab,
             map_tab = map_tab,
             icons_tab = icons_tab,
             tracker_tab = tracker_tab,
             auto_tab = auto_tab,
-            --tooltip_tab = tooltip_tab,
-            --sounds_tab = sounds_tab,
             nameplate_tab = nameplate_tab,
-            dbm_hud_tab = dbm_hud_tab,
             advanced_tab = advanced_tab,
             profiles_tab = LibStub("AceDBOptions-3.0"):GetOptionsTable(Questie.db)
         }
