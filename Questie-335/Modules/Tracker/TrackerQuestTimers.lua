46cab079099b48afac0afdc91c2087f5cae1b211
Aldori <m.baxter1122@yahoo.com>
2026-06-26 09:48:36 -0400

    fix(tracker): Fix quest timer sometimes getting cut off in the Tracker

--- Questie-335/Modules/Tracker/TrackerQuestTimers.lua
+++ Questie-335/Modules/Tracker/TrackerQuestTimers.lua
@@ -138,7 +138,8 @@ function TrackerQuestTimers:GetRemainingTime(quest, frame, clear)
 end
 
 ---@param questId number
----@return string timeRemainingString, number timeRemaining, nil
+---@return string? timeRemainingString @Format is "4 Mins 45 Secs"
+---@return number? timeRemaining
 function TrackerQuestTimers:GetRemainingTimeByQuestId(questId)
     local questLogIndex = GetQuestLogIndexByID(questId)
     if (not questLogIndex) then
@@ -150,29 +151,20 @@ function TrackerQuestTimers:GetRemainingTimeByQuestId(questId)
         return nil
     end
 
-    if type(questTimers) == "number" then
-        local currentQuestLogSelection = GetQuestLogSelection()
-        SelectQuestLogEntry(questLogIndex)
-        -- We can't use GetQuestTimers because we don't know for which quest the timer is.
-        -- GetQuestLogTimeLeft returns the correct value though.
-        local timeRemaining = GetQuestLogTimeLeft(questLogIndex)
-        SelectQuestLogEntry(currentQuestLogSelection)
+    local currentQuestLogSelection = GetQuestLogSelection()
+    SelectQuestLogEntry(questLogIndex)
+    -- We can't use GetQuestTimers because we don't know for which quest the timer is.
+    -- GetQuestLogTimeLeft returns the correct value though.
+    local timeRemaining = GetQuestLogTimeLeft(questLogIndex)
+    SelectQuestLogEntry(currentQuestLogSelection)
 
-        if timeRemaining ~= nil then
-            local timeRemainingString = SecondsToTime(timeRemaining, false, true)
+    if timeRemaining ~= nil then
+        local timeRemainingString = SecondsToTime(timeRemaining, false, false)
 
-            if not strfind(timeRemainingString, "Seconds?") then
-                timeRemainingString = timeRemainingString .. " 0 Seconds"
-            end
-            return timeRemainingString, timeRemaining
-        else
-            return nil
-        end
+        return timeRemainingString, timeRemaining
     else
-        Questie:Error("The return value of GetQuestTimers is not number, something is off. Please report this!")
+        return nil, nil
     end
-
-    return nil
 end
 
 ---@param frame frame|nil
