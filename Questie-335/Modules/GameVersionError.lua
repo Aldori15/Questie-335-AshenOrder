08d58d83a9e3660ffb775ea9063eaf11f833cc4d
Aldori <m.baxter1122@yahoo.com>
2026-06-27 19:42:24 -0400

    fix: make install error fallback self contained

--- Questie-335/Modules/GameVersionError.lua
+++ Questie-335/Modules/GameVersionError.lua
@@ -1,31 +1,33 @@
----@type l10n
-local l10n = QuestieLoader:ImportModule("l10n")
-
--- No timeres or other fancy stuff as 1.12 client is very limited.
+-- No timers or other dependencies here. This file is loaded
+-- by Questie.toc when WoW cannot load Questie-335.toc.
 
 -- StaticPopup has very limited width, so text is split to many lines.
 local msg = {
-    l10n("Questie-335 did not load correctly."),
-    l10n("The AddOns folder name is likely wrong."),
+    "Questie-335 did not load correctly.",
+    "The AddOns folder name is likely wrong.",
 
-    l10n("Rename the folder to Questie-335"),
-    l10n("so WoW loads Questie-335.toc."),
+    "Rename the folder to Questie-335",
+    "so WoW loads Questie-335.toc.",
 
-    l10n("Do not rename Questie-335.toc"),
-    l10n("to Questie.toc."),
+    "Do not rename Questie-335.toc",
+    "to Questie.toc.",
 }
 
-StaticPopupDialogs["QUESTIE_VERSION_ERROR"] = {
-    text = "|cffff0000ERROR|r\n" .. msg[1] .. "\n" .. msg[2] .. "\n\n" .. msg[3] .. "\n" .. msg[4] .. "\n\n" .. msg[5] .. "\n" .. msg[6],
-    button2 = "OK",
-    hasEditBox = false,
-    whileDead = true
-}
+if StaticPopupDialogs and StaticPopup_Show then
+    StaticPopupDialogs["QUESTIE_VERSION_ERROR"] = {
+        text = "|cffff0000ERROR|r\n" .. msg[1] .. "\n" .. msg[2] .. "\n\n" .. msg[3] .. "\n" .. msg[4] .. "\n\n" .. msg[5] .. "\n" .. msg[6],
+        button1 = OKAY or "OK",
+        hasEditBox = false,
+        whileDead = true
+    }
 
-StaticPopup_Show("QUESTIE_VERSION_ERROR")
+    StaticPopup_Show("QUESTIE_VERSION_ERROR")
+end
 
-DEFAULT_CHAT_FRAME:AddMessage("---------------------------------")
-DEFAULT_CHAT_FRAME:AddMessage("|cffff0000ERROR|r: |cff42f5ad" .. msg[1] .. " " .. msg[2] .. "|r")
-DEFAULT_CHAT_FRAME:AddMessage("|cffff0000ERROR|r: |cff42f5ad" .. msg[3] .. " " .. msg[4] .. "|r")
-DEFAULT_CHAT_FRAME:AddMessage("|cffff0000ERROR|r: |cff42f5ad" .. msg[5] .. " " .. msg[6] .. "|r")
-DEFAULT_CHAT_FRAME:AddMessage("---------------------------------")
+if DEFAULT_CHAT_FRAME then
+    DEFAULT_CHAT_FRAME:AddMessage("---------------------------------")
+    DEFAULT_CHAT_FRAME:AddMessage("|cffff0000ERROR|r: |cff42f5ad" .. msg[1] .. " " .. msg[2] .. "|r")
+    DEFAULT_CHAT_FRAME:AddMessage("|cffff0000ERROR|r: |cff42f5ad" .. msg[3] .. " " .. msg[4] .. "|r")
+    DEFAULT_CHAT_FRAME:AddMessage("|cffff0000ERROR|r: |cff42f5ad" .. msg[5] .. " " .. msg[6] .. "|r")
+    DEFAULT_CHAT_FRAME:AddMessage("---------------------------------")
+end
